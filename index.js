bd4149fc4efb4d03f37d33cc91d8ea8c184befca
Abhinand <websollabsabhinand@gmail.com>
2023-08-02 16:55:56 +0530

    jwt-added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const userModal = require('./userModel');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 app.use(cors());
@@ -18,6 +19,39 @@ app.post('/register', async (req, res) => {
         console.log(err);
     }
 });
+
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  
+  try {
+    const user = await userModal.findOne({ email: email });
+
+    if (!user) {
+      res.status(400).json({ message: { msgBody: "No user found", msgError: true } });
+    } else {
+      if (password === user.password) {
+        // Create the payload for the JWT
+        const payload = {
+          email: email
+        };
+
+        // Sign the token with your secret key
+        const secretKey = 'secret_key'; 
+        const token = jwt.sign(payload, secretKey, { expiresIn: '1h' });
+
+        // Send the token in the response
+        res.status(200).json({ isAuthenticated: true, user: { email: email }, token: token });
+      } else {
+        res.status(400).json({ message: { msgBody: "Wrong password", msgError: true } });
+      }
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: { msgBody: "Error has occurred", msgError: true } });
+  }
+});
+  
 
 const port = 5000;
 app.listen(port, () => {
